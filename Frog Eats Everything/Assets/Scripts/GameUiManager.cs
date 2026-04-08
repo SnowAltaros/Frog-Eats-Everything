ebdffa073a32a4f1cfe5a8ab9663cc83cc1b3913
SnowAltaros <p.stas94@icloud.com>
2026-04-08 21:28:17 +0100

    Updates...

--- Frog Eats Everything/Assets/Scripts/GameUiManager.cs
+++ Frog Eats Everything/Assets/Scripts/GameUiManager.cs
@@ -1,35 +1,19 @@
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameUiManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI wingsText;
-    [SerializeField] private GameObject upgradesPanel;
-    [SerializeField] private GameObject upgradeButton;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         wingsText.text = "0";
-        upgradesPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         wingsText.text = "" + PlayerStats.wings;
-    }
-
-    public void OpenUprgradesPanel()
-    {
-        upgradesPanel.SetActive(true);
-        upgradeButton.SetActive(false);
-    }
-
-    public void CloseUpgradesPanel()
-    {
-        upgradesPanel.SetActive(false);
-        upgradeButton.SetActive(true);
     }
 }
